9fdeccc4a0247bec95791d1684b68bf437bf4299
luo <yes_wecode@163.com>
2024-12-31 15:22:08 +0800

    add: init

---
+++ cms-vue3/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { RouterView } from 'vue-router'
+// import { getToken,setToken,removeToken } from '@/utils/token';
+import {get,post} from '@/utils/request'
+
+// get({
+//    url:'/gettest',
+// // 触发401状态
+//    data:{
+//     test:'err'
+//    }
+//   }).then(res=>{
+//     console.log(res);
+//   })
+
+// get({
+// 触发404状态
+//    url:'/test123',
+//   }).then(res=>{
+//     console.log(res);
+//   })
+
+// post({
+//   url:'/posttest',
+// }).then(res=>{
+//   console.log(res);
+// })
+
+
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped> 
+
+</style> 
---
+++ cms-vue3/src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<!-- todo 用于测试布局 -->
+<template>
+  <div>About</div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
