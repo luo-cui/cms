f8c27260e82dd5ef2535b74336e5fe531b8ec475
luo <yes_wecode@163.com>
2024-12-31 18:35:43 +0800

    add: register elemnetUI

---
+++ cms-vue3/src/main.js
@@ -0,0 +1,28 @@
+// import './assets/main.css' 
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+// 引入 element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'    // 越放在下面的css文件，权重会盖过上面的css文件
+import './assets/main.css'
+// 注册所有图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+// 使用 use注册 ElementPlus 插件
+app.use(ElementPlus)
+// 全局注册el 图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+
+// 它告诉 Vue 开始渲染整个应用程序，并将其插入到页面中具有 id="app" 的 DOM 元素内
+app.mount('#app')
