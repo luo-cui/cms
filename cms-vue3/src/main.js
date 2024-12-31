0e81f15543716c279bd7688bc18274a24d5e3ba3
luo <yes_wecode@163.com>
2024-12-31 18:40:34 +0800

    update: 注释

--- cms-vue3/src/main.js
+++ cms-vue3/src/main.js
@@ -20,9 +20,11 @@ app.use(router)
 // 使用 use注册 ElementPlus 插件
 app.use(ElementPlus)
 // 全局注册el 图标
+// 返回一个数组：返回一个包含给定对象自身所有可枚举属性的多个 [key, value] 对的数组
+// app.component(key, component) params：组件名字、组件对象
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
   }
 
 // 它告诉 Vue 开始渲染整个应用程序，并将其插入到页面中具有 id="app" 的 DOM 元素内
-app.mount('#app')
+app.mount('#app') 
